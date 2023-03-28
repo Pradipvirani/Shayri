974c15dd6265b81d1258cb9ee64c42dcdf10939f
Shayri <viranipradip100@gmail.com>
2023-03-28 12:01:44 +0530

    four commit

--- app/src/main/java/com/example/shayri/Custom_Adapter2.java
+++ app/src/main/java/com/example/shayri/Custom_Adapter2.java
@@ -1,5 +1,6 @@
 package com.example.shayri;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -8,33 +9,44 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class Custom_Adapter2 extends BaseAdapter {
-    MainActivity mainActivity;
-    int[] image;
+    MainActivity2 mainActivity2;
+    int image;
     String[] type;
     ListView lv;
     ImageView img;
     TextView txt;
 
     public Custom_Adapter2(MainActivity2 mainActivity2, String[] lvshayri, int image) {
+        this.mainActivity2 = mainActivity2;
+        this.type=lvshayri;
+        this.image=image;
+
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return type.length;
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Object getItem(int i) {
+        return i;
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public long getItemId(int i) {
+        return i;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int i, View view, ViewGroup parent)
+
+    {
+        view= LayoutInflater.from(mainActivity2).inflate(R.layout.s1,parent,false);
+        txt=view.findViewById(R.id.s1_txt);
+        img=view.findViewById(R.id.s1_img);
+        txt.setText(""+type[i]);
+        img.setImageResource(image);
+         return view;
     }
 }
